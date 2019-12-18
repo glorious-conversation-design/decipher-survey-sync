9757ae7b291e52ee54c1adaf189461a179343ce1
George Oancea <george@glorious.agency>
2019-12-18 14:58:09 +0100

    version 0.1

--- bin/index.js
+++ bin/index.js
@@ -5,59 +5,11 @@ const colors = require('colors');
 const inquirer = require('inquirer');
 const dss = require('../lib/decipher-survey-sync.js');
 const APITool = require("../lib/api-tool");
-
-/* when you're ready to start saving versions of survey
-
-DSS workflow
-0. Check if API_Key exists. If true -> make a test api call : If false, prompt user for API_Key, store it and go back to 0.
-1. Prompt user for project number.
- or check if user is already in a project
-
-2. Check if project exists
-	if false => go to 1
-3. Check if project folder with name = project number. If false => Create it and the archive folder
-4. Change directory to project folder.
-5. Download survey.xml into project folder.
-6. Start XML Sync
-7. Listen for changes in survey.xml
- Every time a change occurs, make pull request to api and add current version to archive.
-
-*/
+const fss = require('fs').promises;
+let the_api_key = '';
 
 
-//==>0
-//check api_key
-
-
-
-
-
-
-
-
-/*
-
-
-
-return;
-
-var params = {};
-(
-    params.api_key = await dss.get_api_key();
-)
-
-console.log(params);
-
-return;
-let testParams = {
-    api_key: 'yvcdf3m4b93avrg24b6j8fmqh3vh0w300mvkszyp9b6163277839kh0m0k105hcd',
-    project_number: 'tooltest'
-};
-*/
-
-
-
-start();
+const hep = init();
 
 
 
@@ -81,35 +33,141 @@ start();
 
 
 
-var prompt = inquirer.createPromptModule();
-const questions =
-[{
-    type: 'list',
-    name: 'action',
-    message: 'Welcome to Decipher Survey Sync. Please choose an action!',
-    choices: [ 'Test', 'Select Project', 'Sync' ],
-    validate: (input) => {
 
+
+
+
+
+    // -> get stored api key || ask for one
+
+
+//    const stuff = await init();
+
+//    console.log('but still');
+    //const some_response_i_will_get_at_a_point = await dss.get_api_key();
+    //console.log(some_response_i_will_get_at_a_point);
+
+
+
+    /*
+
+    if (API_KEY == false) {
+
+        console.log('Invalid API Key');
     }
-}]
 
-dss.get_api_key(function(api_key) {
-    if (api_key === undefined) {
-        console.log("Weird!");
+
+    console.log('trying to test');
+
+    test_api_key(API_KEY);
+    */
+
+
+
+
+
+async function init() {
+
+
+    the_api_key = await dss.get_api_key();
+
+
+    if (the_api_key === undefined) {
         return;
     }
 
-    show_menu();
-});
+
+    var prompt = inquirer.createPromptModule();
+    console.log('\n');
+    console.log('\n');
+
+    console.log('\n');
+
+    let choices = ['New Project'];
+    let projectlist = [];
+    try {
+        projectlist = await fss.readdir('projects/');
+    }
+    catch(e) {
+        console.log('Exception : ', e);
+    }
+    projectlist.map((project) => {
+        if (project.substr(0,1) != '.') {
+            choices.push(project);
+        }
+    });
 
 
+
+    const questions =
+    [{
+        type: 'list',
+        name: 'action',
+        message: colors.green('Please choose a project...'),
+        choices: choices,
+        validate: (input) => {
+
+        }
+    }]
+
+
+
+/*  Maybe change it back sometimes but for now I want to show the existing projects here.
+    const questions =
+    [{
+        type: 'list',
+        name: 'action',
+        message: colors.green('Please choose an action...'),
+        choices: [ 'New Project', 'Sync' ],
+        validate: (input) => {
+
+        }
+    }]
+    */
+    console.log('--------------------------------')
+    console.log("Welcome to Decipher Survey Sync!");
+    console.log('--------------------------------')
+
+    console.log('\n');
+    console.log("\n");
+    const {action} = await prompt(questions);
+    let project_number = null;
+    switch(action) {
+        case "New Project":
+            project_number = await dss.download_survey(the_api_key);
+            break;
+        default:
+            project_number = await dss.download_survey(the_api_key, action);
+    }
+    if (project_number === undefined || project_number === null) {
+        //console.error('Could not find the project ');
+        return;
+    }
+    const testParams = {
+        api_key: the_api_key,
+        project_number: project_number,
+    }
+
+    let api_tool = new APITool(testParams);
+
+    api_tool.watch();
+
+}
+
+
+/*
 function show_menu(api_key) {
     console.log("Howdy! ");
     prompt(questions)
     .then(({action}) => {
         switch(action) {
             case "Select Project":
-                dss.download_survey(api_key,show_menu);
+                await dss.download_survey(api_key,show_menu);
+                let api_tool = new APITool(testParams);
+
+
+                api_tool.watch();
+
                 break;
             default:
                 console.log("Action " + action + " not set yet.");
@@ -117,42 +175,16 @@ function show_menu(api_key) {
 
     });
 }
-
-
-
-
-
-async function start() {
-    // -> get stored api key || ask for one
-
-    let API_KEY = dss.get_api_key();
-
-    if (API_KEY == false) {
-
-        console.log('Invalid API Key');
-    }
-    // -> test api -> if false -> reset and start over with new api_key
-
-
-
-    test_api_key(API_KEY);
-
-
-}
-
-
-
-
-
+*/
 async function test_api_key(api_key) {
     const testlink = 'https://glorious.focusvision.com/api/v1/surveys/selfserve/53c/tooltest/files/survey.xml';
     console.log('testing api_key: ' + api_key);
     let response = null;
     try {
         const theheaders = {'x-apikey': api_key};
-
+        console.log(theheaders)
         response = await axios.get(testlink,{
-            headers: {'x-apikey': api_key}
+            headers: theheaders
         });
         console.log('Starting sync tool...');
         show_menu(api_key);
