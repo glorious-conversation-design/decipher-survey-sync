97f6be09ce0c093e85d790d3ba8aee3a33cc2fb6
Boon <andreas.boon@epiphany-rbc.com>
2020-01-20 13:42:32 +0100

    Fix

--- start.cmd
+++ start.cmd
@@ -1,1 +1,11 @@
-git pull & npm install & cls & "C:\Program Files\nodejs\node.exe" decipher-survey-sync.js
+@Echo Off
+
+git pull
+
+Rem if [[ ! -d "node_modules" ]]; then
+    npm install
+Rem fi
+
+Cls
+
+"%ProgramFiles%\nodejs\node.exe" ./decipher-survey-sync.js
