8f4ddd6387c82d1460daeeec2e2a7c473a298132
Boon <andreas.boon@epiphany-rbc.com>
2020-01-21 16:18:33 +0100

    WIP: Restore connection with git if broken.

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-git pull
+if [[ ! -d .git ]]; then
+ git init
+ git remote add origin https://github.com/glorious-conversation-design/decipher-survey-sync.git
+ git clean -fd
+ git pull origin master
+ git branch --set-upstream-to=origin/master master
+else
+ git pull origin master
+fi
 
 # if [[ ! -d "node_modules" ]]; then
     npm install
